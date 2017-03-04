3b251dd7e89fe0e946fdb290652276d7a5b62ea5
SharonShahu <sharon.shahu@gmail.com>
2017-03-04 19:25:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,10 +86,10 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
-    var articleName = res.params.articleName;
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
-
+/*
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
@@ -97,7 +97,7 @@ app.get('/article-two',function(req,res){
 app.get('/article-three',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

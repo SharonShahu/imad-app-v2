b8e7152558b6cce6442dde653175ea02d7bd59ac
SharonShahu <sharon.shahu@gmail.com>
2017-03-04 19:20:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleName',function(req,res){
+app.get('/:articleName',function(req,res){
     var articleName = res.param.articleName;
    res.send(createTemplate(articles[articleName]));
 });

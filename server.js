e8d96ab1ca6fa999122753b040c5b68f35d4f9fd
SharonShahu <sharon.shahu@gmail.com>
2017-03-04 23:58:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,15 @@ app.get('/counter',function(req,res){
    res.send(counter.toString()); 
 });
 
+var names = [];
+app.get('/submit-name',function(req,res){ //URL: /subit-name?name=xxxxx
+  //Get the name from the request
+   var name = req.params.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
+
+
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -104,14 +113,6 @@ app.get('/article-three',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 */
-var names = [];
-app.get('/submit-name/:name',function(req,res){
-  //Get the name from the request
-   var name = req.params.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-});
-
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));

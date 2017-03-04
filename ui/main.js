2803d1984df4aa827c1e35fd79aeaab422e0b1c3
SharonShahu <sharon.shahu@gmail.com>
2017-03-05 00:10:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,25 +31,34 @@ button.onclick = function (){
       if (request.readyState == XMLHttpRequest.DONE){
           //take some action
           if (request.status == 200){
-              var counter = request.responseText;
+              var names = request.responseText;
+              names = JSON.parse(names);
+               var list = '';
+               for (var i=0; i<names.length; i++){
+                   list += '<li>' + names[i] + '</li>';
+               }
+               var ul = document.getElementById("namelist");
+               ul.innerHTML = list;
+            /*  var counter = request.responseText;
               var span = document.getElementById("count");
-              span.innerHTML = counter.toString();
+              span.innerHTML = counter.toString();*/
           }
       }
       //not done yet
   };
   
   //Make the request
-    request.open('GET','http://sharonshahu.imad.hasura-app.io/counter',true);
+   var nameInput = document.getElementById("name");
+ var name = nameInput.value;
+    request.open('GET','http://sharonshahu.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
   
  //Submit name
- var nameInput = document.getElementById("name");
- var name = nameInput.value;
  var submit = document.getElementById("submit_btn");
  submit.onclick = function(){
    //make a request to the server and send the name
+   
    //capture a list of names and render it as a list
    var names = ["name1","name2","name3","name4"];
    var list = '';

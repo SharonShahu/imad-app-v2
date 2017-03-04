6a933bfa3429806cce95d03ee3c76df9bc97eec5
SharonShahu <sharon.shahu@gmail.com>
2017-03-04 23:48:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,9 +28,9 @@ button.onclick = function (){
   
   //Capture the response and store it in a variable
   request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE){
+      if (request.readyState == XMLHttpRequest.DONE){
           //take some action
-          if (request.status === 200){
+          if (request.status == 200){
               var counter = request.responseText;
               var span = document.getElementById("count");
               span.innerHTML = counter.toString();
@@ -45,7 +45,20 @@ button.onclick = function (){
 };
   
  //Submit name
- 
+ var nameInput = document.getElementById("name");
+ var name = nameInput.value;
+ var submit = document.getElementById("submit_btn");
+ submit.onclick = function(){
+   //make a request to the server and send the name
+   //capture a list of names and render it as a list
+   var names = ["name1","name2","name3","name4"];
+   var list = '';
+   for (var i=0; i<names.length; i++){
+       list += '<li>' + names[i] + '</li>';
+   }
+   var ul = document.getElementById("namelist");
+   ul.innerHTML = list;
+ };
  
   //Render the variable in the correct span
 /*  counter = counter + 1;

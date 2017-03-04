0b3c574f9f3ab693c8b8b33d2b3a5d62ce917c40
SharonShahu <sharon.shahu@gmail.com>
2017-03-04 20:18:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
-
+/*
 //Change the text of the main page invoking by ID
 var element = document.getElementById("main-test");
 element.innerHTML = "New";
-
+*/
 //Move the image
 var img = document.getElementById("madi");
 var marginLeft = 0;
@@ -12,7 +12,6 @@ function moveRight(){
     img.style.marginLeft = marginLeft + 'px';
 }
 //every 100ms moveRight function is called
-
-img.onclick = function(){
+img.onclick = function (){
     var interval = setInterval(moveRight,100);
 };

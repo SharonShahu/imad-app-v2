8a1ba31143a8e50ab4149c9a92d957b994508516
SharonShahu <sharon.shahu@gmail.com>
2017-03-09 18:33:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,13 +107,16 @@ app.get('/test-db',function(req,res){
 });
 
 function hash(input,salt){
-    //how do we create a hash
+    //how do we create a hash (password based key derivation function)
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
+    
+    //algorithm: ,d5
+    //'password' -> dhguygywe63873gjhdsbkjfht556gds
 }
 
 app.get('/hash/:input',function(req,res){
-   var hashedString = hash(req.params.input,'this-is-a-random-string'); 
+   var hashedString = hash(req.params.input,'this is a random string'); 
    res.send(hashedString);
 });
 
